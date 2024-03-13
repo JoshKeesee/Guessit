2f11a029b53245e170e326e09ec469bf436b725d
Joshua Keesee <105324950+JoshKeesee@users.noreply.github.com>
2024-03-13 21:13:27 +0000

    Fix timeAgo function

--- public/js/time.js
+++ public/js/time.js
@@ -1,14 +1,21 @@
 const timeAgo = (date) => {
+  const rtf = new Intl.RelativeTimeFormat("en", {
+    numeric: "auto",
+    style: "long",
+  });
   const seconds = Math.floor((new Date() - date) / 1000);
-  let interval = Math.floor(seconds / 31536000);
-  if (interval > 1) return `${interval} years ago`;
-  interval = Math.floor(seconds / 2592000);
-  if (interval > 1) return `${interval} months ago`;
-  interval = Math.floor(seconds / 86400);
-  if (interval > 1) return `${interval} days ago`;
-  interval = Math.floor(seconds / 3600);
-  if (interval > 1) return `${interval} hours ago`;
-  interval = Math.floor(seconds / 60);
-  if (interval > 1) return `${interval} minutes ago`;
-  return `${Math.floor(seconds)} seconds ago`;
+  const c = {
+    year: 31536000,
+    month: 2592000,
+    day: 86400,
+    hour: 3600,
+    minute: 60,
+    second: 1,
+  };
+  for (let i = 0; i < Object.keys(c).length; i++) {
+    const v = Object.keys(c)[i],
+      j = Math.floor(seconds / c[v]);
+    if (j >= 1) return rtf.format(-j, v);
+  }
+  return "just now";
 };
