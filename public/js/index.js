28ce2c5975ae8ea0c0b9197b181c39bf27e953e8
Joshua Keesee <105324950+JoshKeesee@users.noreply.github.com>
2024-03-27 20:33:22 +0000

    feat: Update CSS to adjust positioning and overflow properties, add new quiz questions with multiple choice answers and update HTML content to enhance user experience

--- public/js/index.js
+++ public/js/index.js
@@ -20,12 +20,48 @@
           type: "multiple",
           id: 1,
         },
+        {
+          question: "What is the capital of Germany?",
+          answers: ["Berlin"],
+          answer_choices: ["Paris", "Beijing", "Hungary", "Berlin"],
+          type: "multiple",
+          id: 2,
+        },
+        {
+          question: "What is the capital of Spain?",
+          answers: ["Madrid"],
+          answer_choices: ["Paris", "Beijing", "Hungary", "Madrid"],
+          type: "multiple",
+          id: 3,
+        },
+        {
+          question: "What is the capital of Italy?",
+          answers: ["Rome"],
+          answer_choices: ["Paris", "Rome", "Hungary", "Berlin"],
+          type: "multiple",
+          id: 4,
+        },
+        {
+          question: "What is the capital of Japan?",
+          answers: ["Tokyo"],
+          answer_choices: ["Tokyo", "Beijing", "Hungary", "Berlin"],
+          type: "multiple",
+          id: 5,
+        },
+        {
+          question: "What is the capital of China?",
+          answers: ["Beijing"],
+          answer_choices: ["Beijing", "Belgium", "Hungary", "Berlin"],
+          type: "multiple",
+          id: 6,
+        },
       ],
     },
     settings: {
       randomizeAnswers: true,
     },
   };
+  game.pack.questions = shuffle(game.pack.questions);
   const q = createQuestion(game.pack.questions[0]);
   document.querySelector("#questions").appendChild(q);
 })();
